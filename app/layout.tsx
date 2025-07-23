0860eb4aa0dcfb449f020ea913f45f8e78c85468
unknown <afrizal.aminulloh@gmail.com>
2025-07-23 08:50:08 +0700

    speed vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const interFont = Inter({
   variable: "--font-inter",
@@ -46,6 +47,7 @@ export default function RootLayout({
         {children}
         <ScrollToTop />
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
